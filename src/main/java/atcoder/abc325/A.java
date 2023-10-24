90e8493a277292d1f68272b94a95ea49ea671c99
burnetz <32961116+burnetz@users.noreply.github.com>
2023-10-24 11:23:28 +0900

    ABC325 A AC

---
+++ src/main/java/atcoder/abc325/A.java
@@ -0,0 +1,13 @@
+package atcoder.abc325;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next() + " san" );
+    }
+}
