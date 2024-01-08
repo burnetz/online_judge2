a2397992cdce4d2da41379cd34a2296191c2fa86
burnetz <32961116+burnetz@users.noreply.github.com>
2024-01-08 11:29:28 +0900

    ABC335 A AC

---
+++ src/main/java/atcoder/abc335/A.java
@@ -0,0 +1,11 @@
+package atcoder.abc335;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.substring(0, str.length() - 1) + 4);
+    }
+}
