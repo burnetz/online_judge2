646b9a74f71f71550c6492ba5c35645ab47a1ad5
burnetz <32961116+burnetz@users.noreply.github.com>
2024-03-31 23:39:10 +0900

    ABC347 B AC

---
+++ src/main/java/atcoder/abc347/B.java
@@ -0,0 +1,24 @@
+
+package atcoder.abc347;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class B {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < str.length(); i++) {
+            for(int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
