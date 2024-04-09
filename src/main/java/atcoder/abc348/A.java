6f11aa14964aadd9d93ad726f880ec3d28f7c227
burnetz <32961116+burnetz@users.noreply.github.com>
2024-04-09 14:48:24 +0900

    ABC348 A AC

---
+++ src/main/java/atcoder/abc348/A.java
@@ -0,0 +1,21 @@
+package atcoder.abc348;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            if(i % 3 == 2) {
+                System.out.print("x");
+            }
+            else {
+                System.out.print("o");
+            }
+        }
+
+    }
+}
