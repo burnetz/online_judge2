b5eabf58fc5fa704e82657b456902623f5889d0f
burnetz <32961116+burnetz@users.noreply.github.com>
2024-10-04 10:25:49 +0900

    ABC372 A AC

---
+++ src/main/java/atcoder/abc372/A.java
@@ -0,0 +1,11 @@
+package atcoder.abc372;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf(sc.next().replaceAll("\\.", ""));
+    }
+}
