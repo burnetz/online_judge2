a1db873222514182e61938faee7b06ff575dc9a9
burnetz <32961116+burnetz@users.noreply.github.com>
2025-01-06 14:52:48 +0900

    ABC386 A AC

---
+++ src/main/java/atcoder/abc386/A.java
@@ -0,0 +1,21 @@
+package atcoder.abc386;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = Arrays.stream(new int[4]).map(e -> sc.nextInt()).sorted().toArray();
+
+        if((a[0] == a[1] && a[1] == a[2] && a[2] != a[3])
+        || (a[0] == a[1] && a[1] != a[2] && a[2] == a[3])
+        || (a[0] != a[1] && a[1] == a[2] && a[2] == a[3]) ) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
