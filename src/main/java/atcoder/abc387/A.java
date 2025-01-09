bbd858f9f1ae2dbfeb10c1944e21914e51fe25b5
burnetz <32961116+burnetz@users.noreply.github.com>
2025-01-09 15:40:32 +0900

    ABC387 A AC

---
+++ src/main/java/atcoder/abc387/A.java
@@ -0,0 +1,15 @@
+package atcoder.abc387;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((a + b) * (a + b));
+    }
+}
