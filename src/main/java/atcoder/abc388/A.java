3fd8dd92045932a13a70cde2f6d660fd50204366
burnetz <32961116+burnetz@users.noreply.github.com>
2025-01-14 10:28:15 +0900

    ABC388 A AC

---
+++ src/main/java/atcoder/abc388/A.java
@@ -0,0 +1,11 @@
+package atcoder.abc388;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next().substring(0, 1) + "UPC");
+    }
+}
