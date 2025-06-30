62eb76335487d0cd467ef9e23f31c05e7574c8a3
burnetz <32961116+burnetz@users.noreply.github.com>
2025-06-30 15:57:16 +0900

    ABC412 A AC

---
+++ src/main/java/atcoder/abc412/A.java
@@ -0,0 +1,24 @@
+package atcoder.abc412;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if(a < b) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
