d124fd9524805c1aedbcfd9c71e6b6eeadf75aec
burnetz <32961116+burnetz@users.noreply.github.com>
2026-01-13 13:55:35 +0900

    ABC440 A AC

---
+++ src/main/java/atcoder/abc440/A.java
@@ -0,0 +1,14 @@
+package atcoder.abc440;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println((x << y));
+    }
+}
