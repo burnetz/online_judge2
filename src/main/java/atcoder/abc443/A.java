aa500a519cf186c4b40dc158ea1b5c9d00f8a558
burnetz <32961116+burnetz@users.noreply.github.com>
2026-02-04 11:25:05 +0900

    ABC443 A AC

---
+++ src/main/java/atcoder/abc443/A.java
@@ -0,0 +1,12 @@
+package atcoder.abc443;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next() + "s");
+
+    }
+}
