eb50886219e88fb0a0bea7c611e9580e43d71a7a
burnetz <32961116+burnetz@users.noreply.github.com>
2026-03-10 14:44:59 +0900

    ABC446 A AC

---
+++ src/main/java/atcoder/abc446/A.java
@@ -0,0 +1,14 @@
+package atcoder.abc446;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println("Of" + str.toLowerCase());
+
+    }
+}
