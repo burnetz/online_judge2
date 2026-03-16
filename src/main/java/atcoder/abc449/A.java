cdc22327890f1de0d54db463d6701625d9647671
burnetz <32961116+burnetz@users.noreply.github.com>
2026-03-16 10:35:37 +0900

    ABC449 A AC

---
+++ src/main/java/atcoder/abc449/A.java
@@ -0,0 +1,14 @@
+package atcoder.abc449;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int d = sc.nextInt();
+
+        System.out.println(Math.PI*d*d/4);
+    }
+}
