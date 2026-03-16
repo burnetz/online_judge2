f9979aa7875911c7ce33bca61975e2bdaeb03379
burnetz <32961116+burnetz@users.noreply.github.com>
2026-03-16 16:47:21 +0900

    ABC449 C AC（問題間違いにつき上書き）

--- src/main/java/atcoder/abc449/C.java
+++ src/main/java/atcoder/abc449/C.java
@@ -6,73 +6,31 @@ public class C {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+
         String s = sc.next();
-        String t = sc.next();
 
-        int index1 = 0;
-        int index2 = 0;
-        int count = 0;
-        while(index1 < s.length() && index2 < t.length()) {
-            char c1 = s.charAt(index1);
-            char c2 = t.charAt(index2);
-//            System.out.println("index1 " + index1 + " index2 " + index2);
+        int[][] count = new int[26][n + 1];
 
-            if(c1 == 'A') {
-                if(c2 == 'A') {
-                    index1++;
-                    index2++;
-
-                }
-                else {
-                    count++;
-                    index1++;
-
-                }
-            }
-            else {
-                if(c1 == c2) {
-                    index1++;
-                    index2++;
-                }
-                else if(c2 == 'A') {
-                    count++;
-                    index2++;
-
-                }
-                else {
-                    System.out.println(-1);
-                    return;
-
-                }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 26; j++) {
+                count[j][i + 1] = count[j][i];
             }
 
+            count[s.charAt(i) - 'a'][i + 1]++;
         }
 
-        if(index1 < s.length()) {
-            while(index1 < s.length()) {
-                if(s.charAt(index1) == 'A') {
-                    count++;
-                    index1++;
-                }
-                else {
-                    System.out.println(-1);
-                    return;
-                }
-            }
-        }
-        else if(index2 < t.length()) {
-            while(index2 < t.length()) {
-                if(t.charAt(index2) == 'A') {
-                    count++;
-                    index2++;
-                }
-                else {
-                    System.out.println(-1);
-                    return;
-                }
-            }
+        long sum = 0;
+        for(int i = 0; i < n; i++) {
+            int cIndex = s.charAt(i) - 'a';
+
+            int tmp = count[cIndex][Math.max(i + 1 - l, 0)] - count[cIndex][Math.max(i - r, 0)];
+
+            sum += tmp;
         }
 
-        System.out.println(count);
+        System.out.println(sum);
     }
 }
