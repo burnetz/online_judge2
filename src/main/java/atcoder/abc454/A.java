387c06698d929fd0ae6ce3ebe43cea64718422b9
burnetz <32961116+burnetz@users.noreply.github.com>
2026-04-24 13:14:40 +0900

    ABC454 A AC

---
+++ src/main/java/atcoder/abc454/A.java
@@ -0,0 +1,14 @@
+package atcoder.abc454;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+
+        System.out.println(r - l + 1);
+    }
+}
