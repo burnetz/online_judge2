1b5119be9f690ba4760c9d47e344738a99a0b87b
burnetz <32961116+burnetz@users.noreply.github.com>
2026-04-26 15:01:38 +0900

    ABC455 A AC

---
+++ src/main/java/atcoder/abc455/A.java
@@ -0,0 +1,15 @@
+package atcoder.abc455;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a != b && b == c ? "Yes" : "No");
+    }
+}
