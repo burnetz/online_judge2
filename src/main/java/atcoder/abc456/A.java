6e08abd466270bc705b477616fbb9e801ba0460a
burnetz <32961116+burnetz@users.noreply.github.com>
2026-06-09 15:09:54 +0900

    ABC456 A AC

---
+++ src/main/java/atcoder/abc456/A.java
@@ -0,0 +1,12 @@
+package atcoder.abc456;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        System.out.println(x >= 3 && x <= 18 ? "Yes" : "No");
+    }
+}
