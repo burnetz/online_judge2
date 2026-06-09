d8a379346b033e2b91d85f36eb323a40b96a663c
burnetz <32961116+burnetz@users.noreply.github.com>
2026-06-09 13:55:37 +0900

    ABC461 A AC

---
+++ src/main/java/atcoder/abc461/A.java
@@ -0,0 +1,15 @@
+package atcoder.abc461;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println(a <= d ? "Yes" : "No");
+    }
+}
