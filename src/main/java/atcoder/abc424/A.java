10b85a3c094ab26de7d79de0b784c282bad5efd9
burnetz <32961116+burnetz@users.noreply.github.com>
2026-06-16 17:47:15 +0900

    ABC424 A AC

---
+++ src/main/java/atcoder/abc424/A.java
@@ -0,0 +1,21 @@
+package atcoder.abc424;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a == b || b == c || c == a) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+
+    }
+}
