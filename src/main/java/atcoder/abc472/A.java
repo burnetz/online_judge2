aa94d48c25862bb73d426a3082b4f54380098f68
burnetz <32961116+burnetz@users.noreply.github.com>
2026-08-25 10:29:28 +0900

    ABC472 A AC

---
+++ src/main/java/atcoder/abc472/A.java
@@ -0,0 +1,16 @@
+
+package atcoder.abc472;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next().replaceAll("[B-Z]", ".");
+
+        System.out.println(str);
+
+
+    }
+}
